953b1c7b5d72b6eb31266a7eb4f6e9b49290703b
dineshsharma02 <62809636+dineshsharma02@users.noreply.github.com>
2022-01-23 23:33:15 +0530

    Ready Only Model View Set

---
+++ gs15/api/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from .models import Student
+from .serializers import StudentSerializer
+from rest_framework import viewsets,status
+from rest_framework.response import Response
+
+from api import serializers
+
+
+# Create your views here.
+class StudentReadOnlyModelViewset(viewsets.ReadOnlyModelViewSet):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+
+            
---
+++ gs15/drf15/urls.py
@@ -0,0 +1,16 @@
+
+
+from django.contrib import admin
+from django.urls import path,include
+
+from api import views
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+
+router.register('studentapi',views.StudentReadOnlyModelViewset,basename='student')
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('',include(router.urls))
+]
