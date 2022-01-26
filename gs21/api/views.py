b11a166ca333f589e1af420ca12263b09af3b697
dineshsharma02 <62809636+dineshsharma02@users.noreply.github.com>
2022-01-26 10:33:01 +0530

    Custom auth by overriding basicauth

---
+++ gs21/api/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+from .models import Student
+from .serializers import StudentSerializer
+from rest_framework import viewsets,status
+from rest_framework.response import Response
+# from rest_framework.authentication import TokenAuthentication
+from .customauth import CustomAuth
+from rest_framework.permissions import IsAuthenticated
+
+
+from api import serializers
+
+
+# Create your views here.
+class StudentModelViewset(viewsets.ModelViewSet):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+    # LOCAL AUTHENTICATION USING BasicAuthentication
+    # IN SETTINGS.PY FILE WE HAVE PLACED GLOBAL AUTHENTICATION
+    authentication_classes = [CustomAuth]
+
+    permission_classes = [IsAuthenticated] 
+            
