adbd387a7e5e8b745c86651c121ab97adecbd642
dineshsharma02 <62809636+dineshsharma02@users.noreply.github.com>
2022-01-26 09:34:41 +0530

    creating token & fetching using custom auth class

---
+++ gs19/api/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+from .models import Student
+from .serializers import StudentSerializer
+from rest_framework import viewsets,status
+from rest_framework.response import Response
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
+
+
+from api import serializers
+from .customauth import MyPermission
+
+
+# Create your views here.
+class StudentModelViewset(viewsets.ModelViewSet):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+    # LOCAL AUTHENTICATION USING BasicAuthentication
+    # IN SETTINGS.PY FILE WE HAVE PLACED GLOBAL AUTHENTICATION
+    authentication_classes = [TokenAuthentication]
+
+    permission_classes = [MyPermission] 
+            
---
+++ gs19/drf19/urls.py
@@ -0,0 +1,20 @@
+
+
+from django.contrib import admin
+from django.urls import path,include
+from rest_framework.authtoken.views import obtain_auth_token
+
+from api import views
+from rest_framework.routers import DefaultRouter
+from api.auth import CustomAuthToken
+router = DefaultRouter()
+
+router.register('studentapi',views.StudentModelViewset,basename='student')
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('',include(router.urls)),
+    path('auth/',include('rest_framework.urls',namespace='rest_framework') ),
+    # path('gettoken/',obtain_auth_token)  ## for in built token obtaining mechanism
+    path('gettoken/',CustomAuthToken.as_view())
+]
